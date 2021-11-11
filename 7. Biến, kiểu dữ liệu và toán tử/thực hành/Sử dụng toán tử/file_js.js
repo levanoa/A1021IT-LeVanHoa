f29edea6f2260309b8c30a298cf2c5f4fdd2a0b2
levanoa <lehoa13n.cit@gmail.com>
2021-11-11 18:19:40 +0700

    push thuc hanh toan tu

---
+++ 7. Biến, kiểu dữ liệu và toán tử/thực hành/Sử dụng toán tử/file_js.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHight;
+inputWidth = prompt("Enter the width");
+inputHight = prompt("Enter the hight");
+let width = parseInt(inputWidth);
+let hight = parseInt(inputHight);
+let area = width * hight;
+document.write("the area is: " +area );
