a32d0c4ea381be56c1edccb09e88183b503651c0
levanoa <93359282+levanoa@users.noreply.github.com>
2022-01-04 23:54:19 +0700

    Delete 7. Biến, kiểu dữ liệu và toán tử directory

--- 7. Biến, kiểu dữ liệu và toán tử/thực hành/Sử dụng toán tử/file_js.js
+++
@@ -1,8 +0,0 @@
-let inputWidth;
-let inputHight;
-inputWidth = prompt("Enter the width");
-inputHight = prompt("Enter the hight");
-let width = parseInt(inputWidth);
-let hight = parseInt(inputHight);
-let area = width * hight;
-document.write("the area is: " +area );
