07961acf00975894cfb0f1a65713ce13934c84af
levanhoa <lehoa13n.cit@gmail.com>
2022-01-05 19:42:57 +0700

    modunle2

--- Loop_java/Bai_tap/Prime_less.java
+++ Loop_java/Bai_tap/Prime_less.java
@@ -2,7 +2,7 @@ package Loop_java.Bai_tap;
 
 import java.util.Scanner;
 
-public class Prime {
+public class Prime_less {
     public static boolean checkNumber(int n) {
         if (n < 2) {
             return false;
@@ -15,26 +15,16 @@ public class Prime {
         return true;
     }
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập n = ");
-        int n = scanner.nextInt();
-        System.out.println("Tất cả 20  số nguyên tố đầu tiên là: ");
-        int count = 0;
+        System.out.println("Tất cả số số nguyên tố nhỏ hơn 100 là: ");
+        int n = 100;
         if (n >= 2) {
             System.out.print(2);
         }
         for (int i = 3; i < n; i+=2) {
             if (checkNumber(i)) {
-                count ++;
                 System.out.print(" " + i);
-            }
-            if ( count > 18) {
-                break;
             }
         }
 
-
     }
-
-
 }
