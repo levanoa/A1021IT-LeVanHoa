2a538974a33f3bb3b22a2feb8e147532e897bc86
levanhoa <lehoa13n.cit@gmail.com>
2022-03-22 20:36:14 +0700

    modunle3

---
+++ 1. Cơ sở dữ liệu quan hệ/Bai_tap/XayDungCSDL_Student.sql
@@ -0,0 +1,14 @@
+create database Student;
+
+create table Student.Class (
+   id int,
+   name varchar(30)
+   
+);
+
+create table Student.Teacher (
+     id int,
+     name varchar(30),
+     age int,
+     country varchar(30)
+);
